35ffb6e8dcfaba21edf11ce74af36b1a3ac3390f
Kyy13 <kyy1393@gmail.com>
2022-07-17 20:41:43 -0400

    fixed serialization issue with reliable timestamps. removed network <-> host order conversions for timestamps, because only you will read your own timestamps.

--- src/UdcMessage.cpp
+++ src/UdcMessage.cpp
@@ -49,13 +49,11 @@ namespace serial
     {
         void serializeTimeStamp(uint8_t* msgBuffer, uint32_t timeStamp)
         {
-            timeStamp = htonl(timeStamp);
             memcpy(msgBuffer + msgHeader::SIZE, &timeStamp, sizeof(timeStamp));
         }
 
         void deserializeTimeStamp(const uint8_t* msgBuffer, uint32_t& timeStamp)
         {
-            timeStamp = htonl(timeStamp);
             memcpy(&timeStamp, msgBuffer + msgHeader::SIZE, sizeof(timeStamp));
         }
     }
@@ -75,34 +73,14 @@ namespace serial
 
     namespace msgReliable
     {
-        void serializeEndPointId(uint8_t* msgBuffer, UdcEndPointId endPointId)
-        {
-            memcpy(msgBuffer + msgHeader::SIZE, &endPointId, sizeof(UdcEndPointId));
-        }
-
         void serializeTimeStamp(uint8_t* msgBuffer, uint32_t timeStamp)
         {
-            constexpr uint32_t offset =
-                msgHeader::SIZE +
-                sizeof(UdcEndPointId);
-
-            timeStamp = htonl(timeStamp);
-            memcpy(msgBuffer + offset, &timeStamp, sizeof(timeStamp));
-        }
-
-        void deserializeEndPointId(const uint8_t* msgBuffer, UdcEndPointId& endPointId)
-        {
-            memcpy(&endPointId, msgBuffer + msgHeader::SIZE, sizeof(UdcEndPointId));
+            memcpy(msgBuffer + msgHeader::SIZE, &timeStamp, sizeof(timeStamp));
         }
 
         void deserializeTimeStamp(const uint8_t* msgBuffer, uint32_t& timeStamp)
         {
-            constexpr uint32_t offset =
-                msgHeader::SIZE +
-                sizeof(UdcEndPointId);
-
-            timeStamp = htonl(timeStamp);
-            memcpy(&timeStamp, msgBuffer + offset, sizeof(timeStamp));
+            memcpy(&timeStamp, msgBuffer + msgHeader::SIZE, sizeof(timeStamp));
         }
 
         void serializeData(uint8_t* msgBuffer, const uint8_t* data, uint32_t dataSize)
